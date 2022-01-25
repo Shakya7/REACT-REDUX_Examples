addb616c89214858535901522e7ab277d3e95ee4
Shakya7 <68787887+Shakya7@users.noreply.github.com>
2022-01-26 00:00:37 +0530

    Create Action3.js

---
+++ Ex_1/public/Actions/Action3.js
@@ -0,0 +1,13 @@
+const CHANGE_TEXT="CHANGE_TEXT";
+
+const actionFunc3=()=>{
+  return {
+    type: CHANGE_TEXT
+  }
+}
+export const actionFunc3Reset=()=>{
+  return{
+    type: "RESET"
+  }
+}
+export default actionFunc3;
