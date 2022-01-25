f3d8daa4862a7dc85e0745e83b160a7e7e39236c
Shakya7 <68787887+Shakya7@users.noreply.github.com>
2022-01-26 00:19:44 +0530

    Create App.js

---
+++ Ex_2/src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import {Provider} from "react-redux";
+import store from "./Store/store";
+import Component1 from "./Component1";
+
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <Component1 />
+      </div>
+    </Provider>
+  );
+}
